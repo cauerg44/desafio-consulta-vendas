87441165e89fd5b2db389ba2bf928b57628f8513
Caue Garcia <cauerg7@outlook.com>
2023-12-08 17:05:02 -0300

    getReports, jpql query

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,15 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -19,5 +26,17 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleReportDTO> searchReports(String minDate, String maxDate, String name, Pageable pageable){
+		if(maxDate.isBlank()){
+			maxDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault()).toString();
+		}
+
+		if(minDate.isBlank()){
+			minDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault()).minusYears(1L).toString();
+		}
+
+		return repository.searchSaleReport(LocalDate.parse(minDate), LocalDate.parse(maxDate), name, pageable);
 	}
 }
